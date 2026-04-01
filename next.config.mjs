c1f2842652210ff1e0c2fa31e2cf7126b65fb2a2
Cid Queiroz <cydy.queiroz@gmail.com>
2026-04-01 13:39:13 -0300

    fix(commit): correcao de commit definitivo

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,11 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // Mantendo a transpilação do UI Kit
+  // Mantendo a transpilação do UI Kit caso ele use JSX/TS não compilado
   transpilePackages: ['@cidqueiroz/cdkteck-ui'],
-  // Configuração EXCLUSIVA para Webpack para garantir Singleton do React
-  webpack: (config) => {
-    config.resolve.alias['react'] = path.resolve(__dirname, 'node_modules', 'react');
-    config.resolve.alias['react-dom'] = path.resolve(__dirname, 'node_modules', 'react-dom');
-    return config;
-  },
 };
 
 export default nextConfig;
